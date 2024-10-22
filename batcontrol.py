4610ce0e7e2cb4baad993ae3dc339a638336b707
Matthias Strubel <matthias.strubel@aod-rpg.de>
2024-10-22 15:39:04 +0200

    Move api_overwrite initialization to general init.

--- batcontrol.py
+++ batcontrol.py
@@ -46,6 +46,7 @@ logger.info(f'[Main] Starting Batcontrol ')
 class Batcontrol(object):
     def __init__(self, configfile, is_simulation=False):
         # For API
+        self.api_overwrite = False
         self.last_mode = None  # -1 = charge from grid , 0 = avoid discharge , 10 = discharge allowed 
         self.last_charge_rate = 0
         self.last_prices = None
@@ -102,8 +103,6 @@ class Batcontrol(object):
         self.always_allow_discharge_limit = self.batconfig['always_allow_discharge_limit']     
         self.max_charging_from_grid_limit = self.batconfig['max_charging_from_grid_limit']      
         self.min_price_difference = self.batconfig['min_price_difference'] 
-
-        self.api_overwrite = False
 
         self.mqtt_api = None
         if 'mqtt' in config.keys():
