dc16b9cd760afeba19d0f446f587111e30a49b93
hashtagKnorke <61380298+hashtagKnorke@users.noreply.github.com>
2024-11-17 16:17:01 +0000

    add high price strategy handling

--- heatpump/Thermia.py
+++ heatpump/Thermia.py
@@ -1,13 +1,28 @@
 import inspect
 import logging
 
+from ThermiaOnlineAPI.const import CAL_FUNCTION_EVU_MODE
 from ThermiaOnlineAPI.model.HeatPump import ThermiaHeatPump
+from ThermiaOnlineAPI.model.Schedule import Schedule
 import mqtt_api
 from .baseclass import HeatpumpBaseclass
 from typing import Optional
 
 
-from ThermiaOnlineAPI import Thermia
+from ThermiaOnlineAPI import Thermia 
+from ThermiaOnlineAPI.utils import utils
+from typing import List, Tuple
+from datetime import datetime
+import pytz
+
+class HighPriceHandlingStrategy:
+            def __init__(self, start_time: datetime, end_time: datetime, schedule: Schedule):
+                self.start_time = start_time
+                self.end_time = end_time
+                self.schedule = schedule
+
+            def __repr__(self):
+                return f"HighPriceHandlingStrategy(schedule={self.schedule})"
 
 
 logger = logging.getLogger('__main__')
@@ -17,6 +32,8 @@ logger.info(f'[Heatpump] loading module ')
 class ThermiaHeatpump(HeatpumpBaseclass):
     heat_pump: ThermiaHeatPump = None
     mqtt_api: Optional['mqtt_api.MqttApi'] = None
+    high_price_strategies: dict[datetime, HighPriceHandlingStrategy] = {}
+
 
     def __init__(self, user, password) -> None:
         super().__init__()
@@ -31,6 +48,8 @@ class ThermiaHeatpump(HeatpumpBaseclass):
                 thermia = Thermia(self.user, self.password)
                 logger.debug("Connected: " + str(thermia.connected))
 
+                if not thermia.heat_pumps:
+                    raise Exception("No heat pumps found in account")
                 heat_pump = thermia.heat_pumps[0]
                 self.heat_pump = heat_pump
                 logger.debug("initialized HeatPump" + str(self.heat_pump))
@@ -84,6 +103,46 @@ class ThermiaHeatpump(HeatpumpBaseclass):
         for name, method in inspect.getmembers(obj.__class__, lambda m: isinstance(m, property)):
             yield name, getattr(obj, name)
        
+    def _plan_for_high_price_window(self, start_time: datetime, end_time: datetime):
+        """
+        Plan for high price window.
+
+        This method should be implemented by subclasses to provide the specific
+        logic for planning for high price window.
+
+        Args:
+            start_time (datetime): The start time of the high price window.
+            end_time (datetime): The end time of the high price window.
+        Raises:
+            Error: If the method is not implemented by the subclass.
+        """
+        ## round to full minutes
+        start_time = start_time.replace(second=0, microsecond=0)
+        end_time = end_time.replace(second=0, microsecond=0)
+
+        # Adjust start and end times for time zone of heatpump 
+        tz_name = self.heat_pump.installation_timezone
+        start_time = utils.adjust_times_for_timezone(start_time,tz_name)
+        end_time = utils.adjust_times_for_timezone(end_time,tz_name)
+        
+        duration = end_time - start_time
+        logger.info(f'[ThermiaHeatpump] Planning for high price window starting at {start_time}, duration: {duration}')
+        
+        # Check if a strategy already exists for the given start time
+        if start_time in self.high_price_strategies:
+            existing_strategy = self.high_price_strategies[start_time]
+            logger.info(f'[ThermiaHeatpump] High price handling strategy already exists for start time {start_time}: {existing_strategy}')
+            return
+        
+
+         
+        planned_schedule = Schedule(start=start_time, end=end_time, functionId=CAL_FUNCTION_EVU_MODE)
+        schedule = self.heat_pump.add_new_schedule(planned_schedule)
+        high_price_strategy = HighPriceHandlingStrategy(start_time, end_time, schedule)
+        logger.info(f'[ThermiaHeatpump] Created high price handling strategy: {high_price_strategy}')
+
+        self.high_price_strategies[start_time] = high_price_strategy
+        logger.info(f'[ThermiaHeatpump] Stored high price handling strategy for start time {start_time}')
 
  #   def api_set_max_grid_charge_rate(self, max_grid_charge_rate: int):
  #       if max_grid_charge_rate < 0:
