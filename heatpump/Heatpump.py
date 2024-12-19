96afe2893bb5a1f808f7d5680627a878c6bbb15f
hashtagKnorke <61380298+hashtagKnorke@users.noreply.github.com>
2024-12-19 14:11:41 +0000

    add SilentHeatpump class and enhance Heatpump factory logic

--- heatpump/Heatpump.py
+++ heatpump/Heatpump.py
@@ -1,4 +1,4 @@
-class Heatpump(object):
+class Heatpump(object): 
     """
     Heatpump class factory that returns an instance of a specific heat pump type based on the provided configuration.
 
@@ -7,16 +7,29 @@ class Heatpump(object):
 
     Returns:
         ThermiaHeatpump: If the type specified in the config is 'thermia'.
-        DummyHeatpump: If the type specified in the config is not 'thermia'.
+        DummyHeatpump: If the type specified in the config is 'dummy'.
+        SilentHeatpump: If the type specified in the config is neither 'thermia' nor 'dummy'.
 
     Raises:
         KeyError: If the 'type' key is not present in the config dictionary.
     """
     def __new__(cls, config:dict):
-        if config['type'].lower() == 'thermia':
-            from .Thermia import ThermiaHeatpump
-            return ThermiaHeatpump(config['user'], config['password'])
-        else: 
-            from .DummyHeatpump import DummyHeatpump
-            return DummyHeatpump()
+        if config is None:
+            return cls.default()
+        elif 'type' not in config:
+            return cls.default()
+        else:
+            if config['type'].lower() == 'thermia':
+                from .Thermia import ThermiaHeatpump
+                return ThermiaHeatpump(config['user'], config['password'])
+            elif config['type'].lower() == 'dummy':
+                from .DummyHeatpump import DummyHeatpump
+                return DummyHeatpump()
+            else: 
+                return cls.default()
+        
+    @staticmethod
+    def default():
+        from .SilentHeatpump import SilentHeatpump
+        return SilentHeatpump()    
  
