208bc97ae8a962b8906b70dff4c0172a70690d67
hashtagKnorke <61380298+hashtagKnorke@users.noreply.github.com>
2024-12-28 13:34:12 +0000

    feat: add debug logging for topic deletion process in MQTT_API

--- mqtt_api.py
+++ mqtt_api.py
@@ -347,6 +347,7 @@ class MqttApi:
 
 
     def delete_all_topics(self, prefix: str) -> None:
+        logger.debug(f'[MQTT] Deleting all topics with prefix {prefix}')
         if self.client.is_connected() == True:
             def on_message_delete(client, userdata, message):
                 logger.info(f'[MQTT] Deleting topic {message.topic}')
@@ -354,12 +355,14 @@ class MqttApi:
             
             self.client.message_callback_add(prefix + '/#', on_message_delete)
             self.client.subscribe(prefix + '/#')
+            logger.debug(f'[MQTT] Waiting for messages to be processed')
 
             # Wait for all messages to be processed  
-            time.sleep(2)
+            time.sleep(20)
 
             self.client.unsubscribe(prefix + '/#')
             self.client.message_callback_remove(prefix + '/#')
+            logger.debug(f'[MQTT] All topics with prefix {prefix} deleted')
         return
     
     
