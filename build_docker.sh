464e1a6957dc6bdc5c1d989b94e5e1b00fbf204d
hashtagKnorke <61380298+hashtagKnorke@users.noreply.github.com>
2024-12-29 10:42:26 +0000

    feat: enhance build_docker.sh to include current commit SHA and versioning -- consuned by DOCKERFILE

--- build_docker.sh
+++ build_docker.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
+# Get the current commit SHA
+GIT_SHA=$(git rev-parse HEAD)
+# Get the current Git reference name (branch or tag)
+GIT_REF_NAME=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match)
+
+# Set the VERSION value
+if [ -n "$GIT_REF_NAME" ]; then
+  VERSION=$GIT_REF_NAME
+else
+  VERSION="snapshot"
+fi
+
+# Print the VERSION value
+echo "SHA:$GIT_SHA .. VERSION=$VERSION"
+# Build the Docker image
 docker buildx build . -t hashtagknorke:batcontrol
