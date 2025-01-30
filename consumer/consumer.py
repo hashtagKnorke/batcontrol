5c06959ed48ce7f572ebd025be829e1461f95497
hashtagKnorke <61380298+hashtagKnorke@users.noreply.github.com>
2025-01-30 17:33:36 +0000

    Enhance consumer configuration validation and manifest handling

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -7,51 +7,69 @@ import os
 import yaml
 from typing import List
 
+
 class Consumer:
     """ Factory and registry for consumer providers """
-    # Instances of the consumer classes are created here
+    MANIFEST_FILE_NAME = "manifest.yaml"
+    IMPLEMENTATION_CLASS_KEY = "implementation_class"
     num_consumers = 0
     _pm = pluggy.PluginManager("consumer")
     _pm.add_hookspecs(ConsumerInterface)
     _consumers: List[ConsumerInterface] = []
+    _manifest_dict: dict[str, dict] = {}
 
     @staticmethod
     def create_consumer(config: dict) -> ConsumerInterface:
         """ Select and configure a consumer based on the given configuration """
-        
+
         consumer_name = config.get("name")
         plugin = config.get("plugin")
+
+        # Check if the plugin name is present in the config
         consumer_class = Consumer._pm.get_plugin(name=plugin)
         if not consumer_class:
             raise ValueError(f"No consumer plugin found with name: {plugin}")
-        
-        consumer = consumer_class(config.get("config"))
+
+        consumer_config = config.get("config", {})
+
+        # Check if the required config keys (defined in plugin manifest)
+        # are present in the consumer config
+        manifest = Consumer._manifest_dict.get(plugin)
+        if manifest:
+            required_config = manifest.get("required_config", [])
+            for key in required_config:
+                if key not in consumer_config:
+                    raise ValueError(f"Missing required config key: {key}  for plugin {
+                                     plugin} in consumer config for: {consumer_name}")
+
+        # call constructor of plugin with the config
+        consumer = consumer_class(consumer_config)
         consumer.consumer_num = Consumer.num_consumers
         Consumer.num_consumers += 1
         return consumer
-    
+
     @staticmethod
     def discover_consumers(plugin_base_dir: str):
         """ Discover the available consumer classes """
         Consumer._pm.load_setuptools_entrypoints("consumer")
-        
-        # Manually iterate over all subdirectories of plugin_base_dir
+
+        # iterate over all subdirectories of plugin_base_dir
         for root, _, files in os.walk(plugin_base_dir):
-            if "manifest.yaml" in files:
-                manifest_path = os.path.join(root, "manifest.yaml")
+            if Consumer.MANIFEST_FILE_NAME in files:
+                manifest_path = os.path.join(root, Consumer.MANIFEST_FILE_NAME)
                 with open(manifest_path, "r") as manifest_file:
                     manifest = yaml.safe_load(manifest_file)
-                    implementation_class = manifest.get("implementation_class")
+                    implementation_class = manifest.get(
+                        Consumer.IMPLEMENTATION_CLASS_KEY)
                     if implementation_class:
                         module_name, class_name = implementation_class.rsplit(".", 1)
                         module_path = os.path.join(root, f"{module_name}.py")
                         spec = importlib.util.spec_from_file_location(module_name, module_path)
                         module = importlib.util.module_from_spec(spec)
                         spec.loader.exec_module(module)
-                        Consumer._pm.register(getattr(module, class_name))
-                        if hasattr(module, 'register_plugin'):
-                            module.register_plugin(Consumer._pm)
-                        
+                        consumer_name = Consumer._pm.register(getattr(module, class_name))
+                        Consumer._manifest_dict[consumer_name] = manifest
+
         Consumer._pm.check_pending()
 
     @staticmethod
@@ -67,5 +85,3 @@ class Consumer:
     def get_consumers() -> List[ConsumerInterface]:
         """ Get the list of consumer instances """
         return Consumer._consumers
-
-
